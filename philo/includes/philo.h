b16f271c7532cc6f418a7099b0d1a755f2259de3
Enmanuel Maria Iglesias Garcia Del Castillo <eiglesia@c1r15s3.42madrid.com>
2026-07-13 22:15:32 +0200

    Creación de hilos

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrubio-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: eiglesia <eiglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 20:31:40 by enmanueligl       #+#    #+#             */
-/*   Updated: 2026/07/13 19:36:16 by jrubio-m         ###   ########.fr       */
+/*   Updated: 2026/07/13 21:07:56 by eiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,31 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-
 typedef struct s_philo
 {
 	int				id;
-	struct s_times	*times;
+	struct s_data	*data;
 	pthread_t		thread;
+	pthread_mutex_t	r_fork;
+	pthread_mutex_t	*l_fork;
 }	t_philo;
 
-typedef struct s_times
+typedef struct s_data
 {
 	long long		born_time;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				num_meals;
-}	t_times;
+	int				num_philos;
+}	t_data;
 
 void	print_action(t_philo *philo, char *action);
 void	*day(void *arg);
+int		parseo(int argc, char **argv);
+
+// utils
+
+int		ft_atoi(const char *nptr);
 
 #endif
-
--- philo/src/main.c
+++ philo/src/main.c
@@ -3,28 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enmanueliglesiasgarciadelcastillo <enma    +#+  +:+       +#+        */
+/*   By: eiglesia <eiglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 20:31:43 by enmanueligl       #+#    #+#             */
-/*   Updated: 2026/07/13 18:54:04 by enmanueligl      ###   ########.fr       */
+/*   Updated: 2026/07/13 21:45:07 by eiglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+t_philo	*get_philo(t_data *data)
+{
+	t_philo	*philos;
+	int		i;
+
+	i = 0;
+	philos = malloc(sizeof(t_philo) * data->num_philos);
+	if (!philos)
+		return (printf("Error: Fallo de malloc\n"), NULL);
+	while (i < data->num_philos)
+	{
+		philos[i].id = i + 1;
+		philos[i].data = data;
+		i++;
+	}
+	return (philos);
+}
+
+long long	get_time_ms(void)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		return (-1);
+	return ((long long)time.tv_sec * 1000 + time.tv_usec / 1000);
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo		*philo;
-	t_times		*times;
+	t_philo		*philos;
+	t_data		data;
+	long long	start;
+	long long	end;// testing gettimeofday
 
 	if (parseo(argc, argv) == 1)
 	{
-		times = get_times(argc, argv);
-		philo = get_philo(argc, argv, times);
-
-		philo[0].id = 1;
-		pthread_create(&(philo[0].thread), NULL, day, philo);
-		pthread_join(philo[0].thread, NULL);
+		start = get_time_ms();
+		usleep(250000);
+		end = get_time_ms();
+		printf("%lld, %lld\n", start, end);
+		printf("Han pasado aproximadamente %lld ms\n", end - start);
+		data.num_philos = ft_atoi(argv[1]);
+		data.time_to_die = ft_atoi(argv[2]);
+		data.time_to_eat = ft_atoi(argv[3]);
+		data.time_to_sleep = ft_atoi(argv[4]);
+		data.num_meals = -1;
+		if (argc == 6)
+			data.num_meals = ft_atoi(argv[5]);
+		philos = get_philo(&data);
+		if (!philos)
+			return (0);
+		pthread_create(&(philos[0].thread), NULL, day, philos);
+		pthread_join(philos[0].thread, NULL);
+		free(philos);
 		return (0);
 	}
 }
